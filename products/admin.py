35f072431055cab1a9bfc89b21a05f3679a1da00
NourIssa7 <nouracm7@gmail.com>
2023-11-28 04:04:40 -0500

    admin area

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
-
+from .models import Product, Offer
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name','price')
+
+
+admin.site.register(Product,ProductAdmin)
+
+class OfferAdmin(admin.ModelAdmin):
+    list_display = ('code','Description')
+
+
+admin.site.register(Offer,OfferAdmin)
