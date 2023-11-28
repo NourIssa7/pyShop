6f2a3f247adb82992adec762a12694222dbade18
NourIssa7 <nouracm7@gmail.com>
2023-11-28 12:57:56 -0500

    templates with bootstrap

--- products/views.py
+++ products/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
 
 
 def index (request):
-    return HttpResponse('Hello World') #arguments to the constructor
+    products = Product.objects.all()
+    return render(request,'index.html',{'products':products}) #arguments to the constructor
 
 def new(request):
     return HttpResponse('new products')
